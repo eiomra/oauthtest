a814bbdcc8af009cc532d527aa070de0727b5206
Thompson Oboyi <105569910+tumazfresh@users.noreply.github.com>
2023-07-29 10:07:55 +0100

    Update userdetails.php

--- includes/dskp/userdetails.php
+++ includes/dskp/userdetails.php
@@ -7,8 +7,12 @@
       <div class="col mtp">
         <div class="container text-start">
     <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1"> 
-      <div class="col"> <h1><p>Email: <?php echo $userData['email']; ?></p></h1></div>
-           
+      <div class="col"><h1><p>Welcome <?php echo $userData['username']; ?></p></h1></div>
+      <div class="col">Email: <?php echo $userData['email']; ?></div>
+      <div class="col">Name: <?php echo $userData['first_name']; ?> <?php echo $userData['first_name']; ?></div>
+      <div class="col">Phone: <?php echo $userData['phone_number']; ?> </div>
+      <div class="col">Sex: <?php echo $userData['gender']; ?> </div>
+      <div class="col">Address: <?php echo $userData['address']; ?>, <?php echo $userData['country']; ?> </div>  
     </div>
   </div></div> 
     </div>
