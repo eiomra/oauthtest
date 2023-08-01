bb97fbfd158ce16fc5db70a391c86e7b927e4d81
Thompson Oboyi <tumazfresh@gmail.com>
2023-08-01 03:17:12 +0100

    new updates

--- includes/dskp/userdetails.php
+++ includes/dskp/userdetails.php
@@ -6,11 +6,9 @@
     <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1"> 
       <div class="col"><h1><p>Welcome <?php echo $userData['username']; ?></p></h1></div>
       <div class="col">Email: <?php echo $userData['email']; ?></div>
-      <div class="col">Name: <?php echo $userData['first_name']; ?> <?php echo $userData['first_name']; ?></div>
-      <div class="col">Phone: <?php echo $userData['phone_number']; ?> </div>
-      <div class="col">Sex: <?php echo $userData['gender']; ?> </div>
-      <div class="col">Address: <?php echo $userData['address']; ?>, <?php echo $userData['country']; ?> </div>  
-    </div>
+      <div class="col">First Name: <?php echo $userData['first_name']; ?></div>
+      <div class="col">Last Name: <?php echo $userData['last_name']; ?> </div>
+          </div>
   </div></div> 
     </div>
   </div>
